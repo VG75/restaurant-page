7ada59c52843f309549ddcba69df246323400506
Vageesha G <vageeshag75@gmail.com>
2025-02-06 15:31:53 +0000

    Now the home page gets added ones the home button is clicked the nenessary code for that is in home.js file and it is being imported to index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import "./style.css"; // Add this in src/index.js
+// import "./style.css"; // Add this in src/index.js
+import {renderHome} from "./home.js"
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const contact = document.querySelector("#contact");
+
+
+
+home.addEventListener("click", () => {
+    renderHome();
+});
