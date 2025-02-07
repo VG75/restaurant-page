7092a73a73a72c49f01158eee8c3f9d24ac228ae
Vageesha G <vageeshag75@gmail.com>
2025-02-07 07:23:36 +0000

    Completed the menu injs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // import "./style.css"; // Add this in src/index.js
-import {renderHome} from "./home.js"
+import {renderHome} from "./home.js";
+import {renderMenu} from "./menu.js";
 const home = document.querySelector("#home");
 const menu = document.querySelector("#menu");
 const contact = document.querySelector("#contact");
+const container = document.querySelector("#container")
 
 
 
 home.addEventListener("click", () => {
     renderHome();
+});
+
+menu.addEventListener("click", () => {
+    container.innerHTML = '';
+    const menuElements = renderMenu();
+    container.appendChild(menuElements.heading);
+    container.appendChild(menuElements.div1);
+    container.appendChild(menuElements.div2);
+    container.appendChild(menuElements.div3);
 });
